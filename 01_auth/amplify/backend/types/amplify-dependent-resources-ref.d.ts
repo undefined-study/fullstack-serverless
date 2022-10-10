8a8524f3f0970970c025b630d9aad6a4f02cb016
youthfulhps <ybh942002@gmail.com>
2022-10-11 01:50:58 +0900

    Feature: Create amplify auth example project

---
+++ 01_auth/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyauthproject771abe91": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
